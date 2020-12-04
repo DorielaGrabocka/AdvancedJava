ee47d0ee2f5750641f29d2712f8729f5c9c9e5da
DorielaGrabocka <dorielagrabocka@unyt.edu.al>
2020-12-04 18:43:41 +0100

    This are some Thread and Concurrency excercises

---
+++ src/com/examples/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c)  4/12/2020. Author Doriela Grabocka. All rights reserved.
+ */
+
+package com.examples;
+
+import org.w3c.dom.ls.LSOutput;
+
+import javax.sound.midi.Soundbank;
+
+public class Hello {
+    public  static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
